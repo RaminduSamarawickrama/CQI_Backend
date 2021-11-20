9ca810dbf1a356f3b21ad4eb4085c33323e40000
Ramindu Samarawickrama <ramindu.samarawickrama@gmail.com>
2021-11-20 13:56:57 +0000

    Initial commit

---
+++ src/main/java/com/cqi_backend/CqiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cqi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqi_backend/CqiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
